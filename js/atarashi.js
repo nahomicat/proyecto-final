6ac79bc10ff4dd12e1a705ba447a92029e8fea80
nahomicat <95841783+nahomicat@users.noreply.github.com>
2021-12-09 01:46:08 -0400

    Add files via upload

---
+++ js/atarashi.js
@@ -0,0 +1,130 @@
+// Variables
+
+let nav = document.getElementById('nav');
+let menu = document.getElementById('enlaces');
+let abrir = document.getElementById('open');
+let botones = document.getElementsByClassName('btn-header');
+let cerrado = true;
+
+function menus(){
+    let Desplazamiento_Actual = window.pageYOffset;
+
+    if(Desplazamiento_Actual <= 300){
+        nav.classList.remove('nav2');
+        nav.className = ('nav1');
+        nav.style.transition = '1s';
+        menu.style.top = '80px';
+        abrir.style.color = '#fff';
+    }else{
+        nav.classList.remove('nav1');
+        nav.className = ('nav2');
+        nav.style.transition = '1s';
+        menu.style.top = '100px';
+        abrir.style.color = '#000';
+    }
+}
+
+function apertura(){
+    if(cerrado){
+        menu.style.width = '70vw';
+        cerrado = false;
+    }else{
+        menu.style.width = '0%';
+        menu.style.overflow = 'hidden';
+        cerrado = true;
+    }
+}
+
+window.addEventListener('load', function(){
+    $('#onload').fadeOut();
+    $('body').removeClass('hidden');
+    menus();
+});
+window.addEventListener('click',function(e){
+    console.log(e.target);
+    if(cerrado==false){
+        let span = document.querySelector('span');
+        if(e.target !== span && e.target !== abrir){
+            menu.style.width = '0%';
+            menu.style.overflow = 'hidden';
+            cerrado = true;
+        }
+    }
+});
+window.addEventListener('scroll', function(){
+    console.log(window.pageYOffset);
+    menus();
+});
+window.addEventListener('resize', function(){
+    if(screen.width>= 700){
+        cerrado = true;
+        menu.style.removeProperty('overflow');
+        menu.style.removeProperty('width');
+    }
+});
+abrir.addEventListener('click', function(){
+    apertura();
+});
+
+
+//filtro
+
+$(function () {
+    $('.filter').click(function () {
+        $(this).addClass('active').siblings().removeClass('active');
+        let valor = $(this).attr('data-nombre');
+        if (valor == 'todos') {
+            $('.cont-work').show('1000');
+        } else {
+            $('.cont-work').not('.' + valor).hide('1000');
+            $('.cont-work').filter('.' + valor).show('1000');
+        }
+    });
+
+    let equipo = $('#equipo').offset().top,
+        servicio = $('#servicio').offset().top,
+        trabajo = $('#trabajo').offset().top,
+        contacto = $('#contacto').offset().top;
+
+    window.addEventListener('resize', function(){
+        let equipo = $('#equipo').offset().top,
+        servicio = $('#servicio').offset().top,
+        trabajo = $('#trabajo').offset().top,
+        contacto = $('#contacto').offset().top;
+    });
+
+    $('#enlace-inicio').on('click', function(e){
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: 0
+        },600);
+    });
+
+    $('#enlace-equipo').on('click', function(e){
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: equipo -100
+        },600);
+    });
+
+    $('#enlace-servicio').on('click', function(e){
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: servicio -100
+        },600);
+    });
+
+    $('#enlace-trabajo').on('click', function(e){
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: trabajo -100
+        },600);
+    });
+
+    $('#enlace-contacto').on('click', function(e){
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: contacto -100
+        },600);
+    });
+});
